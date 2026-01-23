8dc20b280721469287f0ae98c11993f263116095
Keresztes Zsolt <77016531+kereszteszsolt@users.noreply.github.com>
2026-01-23 16:00:02 +0200

    refactor: update router history and set base URL for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'url';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/queens-puzzle-generator/',
   plugins: [vue()],
   resolve: {
     alias: {
